4625d525dd5fa8f072b3fea1a656b05862070d6f
Galileu Mendes <galileu@mendes>
2019-08-25 17:39:56 -0300

    Adição de classes do modelo

---
+++ Java/checkin-app/src/main/java/br/sebrae/ce/checkinapp/model/Person.java
@@ -0,0 +1,67 @@
+package br.sebrae.ce.checkinapp.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Person {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	private String name;
+	private String cpf;
+	private String cnpj;
+	private String dateBirth;
+	private String phoneNumber;
+	@ManyToMany(cascade = CascadeType.ALL)
+	private List<Presence> presences;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getDateBirth() {
+		return dateBirth;
+	}
+	public void setDateBirth(String dateBirth) {
+		this.dateBirth = dateBirth;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public List<Presence> getPresences() {
+		return presences;
+	}
+	public void setPresences(List<Presence> presences) {
+		this.presences = presences;
+	}
+}
